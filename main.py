591f42cbc133d5ce0b58d4553019d4d5ad2df3c9
Noertri <trinuruns@gmail.com>
2024-12-18 10:46:00 +0700

    tes menampilkan data

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 # This Python file uses the following encoding: utf-8
 import sys
+import os
 
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QHeaderView
+from PySide6.QtCore import Qt
+from PySide6 import QtCore
 
 # Important:
 # You need to run the following command to generate the ui_form.py file
 #     pyside6-uic form.ui -o ui_form.py, or
 #     pyside2-uic form.ui -o ui_form.py
-from ui_form import Ui_MainWindow
 
-class Widget(QWidget):
+from main_ui import Ui_MainWindow
+
+
+class TableModel(QtCore.QAbstractTableModel):
+
+    def __init__(self, data, parent = None):
+        super().__init__(parent)
+        self._data = data or []
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            return self._data[index.row()][index.column()]
+        
+    def rowCount(self, parent = None):
+        return len(self._data)
+    
+    def columnCount(self, parent = None):
+        return 2
+
+
+class MainWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.model = TableModel(list(os.environ.items()))
+        self.ui.table.setModel(self.model)
+        self.ui.btn_close.clicked.connect(lambda : self.close())
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    widget = Widget()
+    widget = MainWidget()
     widget.show()
     sys.exit(app.exec())
---
+++ main_ui.py
@@ -0,0 +1,220 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractItemView, QApplication, QFrame, QHBoxLayout,
+    QHeaderView, QLayout, QPushButton, QSizePolicy,
+    QSpacerItem, QTableView, QVBoxLayout, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.setWindowModality(Qt.NonModal)
+        MainWindow.resize(600, 400)
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setMaximumSize(QSize(600, 400))
+        palette = QPalette()
+        brush = QBrush(QColor(0, 0, 0, 255))
+        brush.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.WindowText, brush)
+        brush1 = QBrush(QColor(222, 221, 218, 255))
+        brush1.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Button, brush1)
+        brush2 = QBrush(QColor(255, 255, 255, 255))
+        brush2.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Light, brush2)
+        brush3 = QBrush(QColor(238, 238, 236, 255))
+        brush3.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Midlight, brush3)
+        brush4 = QBrush(QColor(111, 110, 109, 255))
+        brush4.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Dark, brush4)
+        brush5 = QBrush(QColor(148, 147, 145, 255))
+        brush5.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.Mid, brush5)
+        palette.setBrush(QPalette.Active, QPalette.Text, brush)
+        palette.setBrush(QPalette.Active, QPalette.BrightText, brush2)
+        palette.setBrush(QPalette.Active, QPalette.ButtonText, brush)
+        palette.setBrush(QPalette.Active, QPalette.Base, brush2)
+        palette.setBrush(QPalette.Active, QPalette.Window, brush1)
+        palette.setBrush(QPalette.Active, QPalette.Shadow, brush)
+        palette.setBrush(QPalette.Active, QPalette.AlternateBase, brush3)
+        brush6 = QBrush(QColor(255, 255, 220, 255))
+        brush6.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Active, QPalette.ToolTipBase, brush6)
+        palette.setBrush(QPalette.Active, QPalette.ToolTipText, brush)
+        brush7 = QBrush(QColor(0, 0, 0, 127))
+        brush7.setStyle(Qt.SolidPattern)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Active, QPalette.PlaceholderText, brush7)
+#endif
+        palette.setBrush(QPalette.Inactive, QPalette.WindowText, brush)
+        brush8 = QBrush(QColor(239, 239, 239, 255))
+        brush8.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Inactive, QPalette.Button, brush8)
+        palette.setBrush(QPalette.Inactive, QPalette.Light, brush2)
+        brush9 = QBrush(QColor(202, 202, 202, 255))
+        brush9.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Inactive, QPalette.Midlight, brush9)
+        brush10 = QBrush(QColor(159, 159, 159, 255))
+        brush10.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Inactive, QPalette.Dark, brush10)
+        brush11 = QBrush(QColor(184, 184, 184, 255))
+        brush11.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Inactive, QPalette.Mid, brush11)
+        palette.setBrush(QPalette.Inactive, QPalette.Text, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.BrightText, brush2)
+        palette.setBrush(QPalette.Inactive, QPalette.ButtonText, brush)
+        palette.setBrush(QPalette.Inactive, QPalette.Base, brush2)
+        palette.setBrush(QPalette.Inactive, QPalette.Window, brush8)
+        brush12 = QBrush(QColor(118, 118, 118, 255))
+        brush12.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Inactive, QPalette.Shadow, brush12)
+        brush13 = QBrush(QColor(247, 247, 247, 255))
+        brush13.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Inactive, QPalette.AlternateBase, brush13)
+        palette.setBrush(QPalette.Inactive, QPalette.ToolTipBase, brush6)
+        palette.setBrush(QPalette.Inactive, QPalette.ToolTipText, brush)
+        brush14 = QBrush(QColor(0, 0, 0, 128))
+        brush14.setStyle(Qt.SolidPattern)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Inactive, QPalette.PlaceholderText, brush14)
+#endif
+        palette.setBrush(QPalette.Disabled, QPalette.WindowText, brush4)
+        palette.setBrush(QPalette.Disabled, QPalette.Button, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Light, brush2)
+        palette.setBrush(QPalette.Disabled, QPalette.Midlight, brush3)
+        palette.setBrush(QPalette.Disabled, QPalette.Dark, brush4)
+        palette.setBrush(QPalette.Disabled, QPalette.Mid, brush5)
+        palette.setBrush(QPalette.Disabled, QPalette.Text, brush4)
+        palette.setBrush(QPalette.Disabled, QPalette.BrightText, brush2)
+        palette.setBrush(QPalette.Disabled, QPalette.ButtonText, brush4)
+        palette.setBrush(QPalette.Disabled, QPalette.Base, brush1)
+        palette.setBrush(QPalette.Disabled, QPalette.Window, brush1)
+        brush15 = QBrush(QColor(177, 177, 177, 255))
+        brush15.setStyle(Qt.SolidPattern)
+        palette.setBrush(QPalette.Disabled, QPalette.Shadow, brush15)
+        palette.setBrush(QPalette.Disabled, QPalette.AlternateBase, brush13)
+        palette.setBrush(QPalette.Disabled, QPalette.ToolTipBase, brush6)
+        palette.setBrush(QPalette.Disabled, QPalette.ToolTipText, brush)
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+        palette.setBrush(QPalette.Disabled, QPalette.PlaceholderText, brush14)
+#endif
+        MainWindow.setPalette(palette)
+        self.verticalLayout = QVBoxLayout(MainWindow)
+        self.verticalLayout.setSpacing(20)
+        self.verticalLayout.setContentsMargins(10, 10, 10, 10)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setSizeConstraint(QLayout.SetDefaultConstraint)
+        self.verticalLayout.setContentsMargins(20, 20, 20, 20)
+        self.table_frame = QFrame(MainWindow)
+        self.table_frame.setObjectName(u"table_frame")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.table_frame.sizePolicy().hasHeightForWidth())
+        self.table_frame.setSizePolicy(sizePolicy1)
+        self.table_frame.setFrameShape(QFrame.NoFrame)
+        self.table_lo = QVBoxLayout(self.table_frame)
+        self.table_lo.setSpacing(20)
+        self.table_lo.setContentsMargins(10, 10, 10, 10)
+        self.table_lo.setObjectName(u"table_lo")
+        self.table_lo.setContentsMargins(20, 20, 20, 20)
+        self.table = QTableView(self.table_frame)
+        self.table.setObjectName(u"table")
+        self.table.setHorizontalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.table.setTabKeyNavigation(False)
+        self.table.setProperty(u"showDropIndicator", False)
+        self.table.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.table.horizontalHeader().setVisible(True)
+        self.table.horizontalHeader().setMinimumSectionSize(200)
+        self.table.horizontalHeader().setDefaultSectionSize(200)
+        self.table.horizontalHeader().setStretchLastSection(True)
+        self.table.verticalHeader().setVisible(False)
+
+        self.table_lo.addWidget(self.table)
+
+        self.btn_lo2 = QHBoxLayout()
+        self.btn_lo2.setSpacing(10)
+        self.btn_lo2.setObjectName(u"btn_lo2")
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.btn_lo2.addItem(self.horizontalSpacer_2)
+
+        self.btn_new = QPushButton(self.table_frame)
+        self.btn_new.setObjectName(u"btn_new")
+
+        self.btn_lo2.addWidget(self.btn_new)
+
+        self.btn_edit = QPushButton(self.table_frame)
+        self.btn_edit.setObjectName(u"btn_edit")
+
+        self.btn_lo2.addWidget(self.btn_edit)
+
+        self.btn_delete = QPushButton(self.table_frame)
+        self.btn_delete.setObjectName(u"btn_delete")
+
+        self.btn_lo2.addWidget(self.btn_delete)
+
+
+        self.table_lo.addLayout(self.btn_lo2)
+
+
+        self.verticalLayout.addWidget(self.table_frame)
+
+        self.btn_lo1 = QHBoxLayout()
+        self.btn_lo1.setSpacing(10)
+        self.btn_lo1.setObjectName(u"btn_lo1")
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.btn_lo1.addItem(self.horizontalSpacer)
+
+        self.btn_ok = QPushButton(MainWindow)
+        self.btn_ok.setObjectName(u"btn_ok")
+        self.btn_ok.setAutoFillBackground(False)
+
+        self.btn_lo1.addWidget(self.btn_ok)
+
+        self.btn_close = QPushButton(MainWindow)
+        self.btn_close.setObjectName(u"btn_close")
+
+        self.btn_lo1.addWidget(self.btn_close)
+
+
+        self.verticalLayout.addLayout(self.btn_lo1)
+
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"Environment Variables", None))
+        self.btn_new.setText(QCoreApplication.translate("MainWindow", u"New", None))
+        self.btn_edit.setText(QCoreApplication.translate("MainWindow", u"Edit", None))
+        self.btn_delete.setText(QCoreApplication.translate("MainWindow", u"Delete", None))
+        self.btn_ok.setText(QCoreApplication.translate("MainWindow", u"OK", None))
+        self.btn_close.setText(QCoreApplication.translate("MainWindow", u"Close", None))
+    # retranslateUi
+
