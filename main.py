7ec85a689b77d9a4daed45aa53a8b3291ac5b336
Noertri <trinuruns@gmail.com>
2024-12-18 21:26:11 +0700

    membuat method init_app

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # This Python file uses the following encoding: utf-8
+import json
+import re
 import sys
 import os
+from pathlib import Path
 
 from PySide6.QtWidgets import QApplication, QWidget, QHeaderView
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, QModelIndex
 from PySide6 import QtCore
 
 # Important:
@@ -18,11 +21,12 @@ class TableModel(QtCore.QAbstractTableModel):
 
     def __init__(self, data, parent = None):
         super().__init__(parent)
-        self._data = data or []
+        self._data = data or {}
 
-    def data(self, index, role):
-        if role == Qt.DisplayRole:
-            return self._data[index.row()][index.column()]
+    def data(self, index: QModelIndex, role: int = Qt.DisplayRole):
+        if index.isValid() and role == Qt.DisplayRole:
+            _data_list = list(self._data.items())
+            return _data_list[index.row()][index.column()]
         
     def rowCount(self, parent = None):
         return len(self._data)
@@ -58,12 +62,163 @@ class MainWidget(QWidget):
         super().__init__(parent)
         self.main_ui = UiMainWindow(self)
 
+        # initialization
+        self.init_app()
+
+    def init_app(self):
+        self.app_workdir = Path("{0}/.local/share/ManageEnv".format(Path.cwd()))
+        self.app_configs = {
+                    "commons": [
+                        "HOME",
+                        "PATH",
+                        "PWD",
+                        "USER",
+                        "SHELL",
+                        "UID",
+                        "HOSTNAME",
+                        "LANG",
+                        "MAIL",
+                        "EDITOR",
+                        "TEMP",
+                        "PS1"
+                    ],
+                    "excludes": [
+                        ".*CURRENT_DESKTOP",
+                        ".*RUNTIME.*",
+                        ".*SESSION.*",
+                        ".*MENU_PREFIX",
+                        "PYSIDE.*",
+                        "GIO.*",
+                        "GDM_LANG",
+                        "WINDOWPATH",
+                        "QT.*",
+                        "LC.*",
+                        "KDE.*",
+                        "GTK.*",
+                        "PAM.*",
+                        "^_.*",
+                        "SESSION_MANAGER",
+                        ".*TERM.*",
+                        "SSH_AUTH_SOCK", 
+                        "XMODIFIERS", 
+                        "DESKTOP_SESSION",
+                        "SSH_AGENT_PID", 
+                        "XCURSOR_SIZE",
+                        "GPG_AGENT_INFO", 
+                        "SYSTEMD_EXEC_PID", 
+                        "XAUTHORITY", 
+                        "VSCODE_GIT_ASKPASS_NODE", 
+                        "IM_CONFIG_PHASE",
+                        "LS_COLORS", 
+                        "VIRTUAL_ENV", 
+                        "GIT_ASKPASS",
+                        "INVOCATION_ID", 
+                        "MANAGERPID", 
+                        "CHROME_DESKTOP", 
+                        "CLUTTER_IM_MODULE", 
+                        "VSCODE_GIT_ASKPASS_EXTRA_ARGS", 
+                        "LESS.*",
+                        "VSCODE_GIT_IPC_HANDLE",
+                        "DISPLAY",
+                        "SHLVL", 
+                        "VIRTUAL_ENV_PROMPT",
+                        "VSCODE_GIT_ASKPASS_MAIN",
+                        "JOURNAL_STREAM", 
+                        "XCURSOR_THEME", 
+                        "GDK_BACKEND",
+                        ".*BUS.*",
+                        ".*DEBUG.*",
+                        ".*ENCODING.*",
+                        ".*BUFFERED.*",
+                        "PYDEVD.*",
+                        "LD.*",
+                        "TK.*",
+                        "TCL.*"
+                    ]
+                }
+        
+        self.env_file_path = Path("{0}/.environment".format(Path.home()))
+        self.app_data = dict()
+
+        if not self.app_workdir.exists():
+            self.app_workdir.mkdir(parents=True)
+
+        if not self.app_workdir.joinpath("config.json").exists():
+            self._create_configs(self.app_workdir.joinpath("config.json"))
+        else:
+            self._load_configs(self.app_workdir.joinpath("config.json"))
+
+        bashrc_path = Path("{0}/.bashrc".format(Path.home()))
+        dotfile_path = Path("{0}/.profile".format(Path.home()))
+        
+        script = 'if [ -f ~/.environment ]; then\n\tset -a\n\tsource ~/.environment\n\tset +a\nfi'
+
+        if not self.env_file_path.exists():
+            self.env_file_path.touch()
+
+            with bashrc_path.open("a", encoding="utf-8") as f:
+                f.write("\n"+script.strip()+"\n")
+                f.close()
+
+            with dotfile_path.open("a", encoding="utf-8") as f:
+                f.write("\n"+script.strip()+"\n")
+                f.close()
+
+        env_os = dict(os.environ)
+        env_os = dict(filter(self._filter_vars, env_os.items()))
+        env_os.update(self.load_env_file())
+
+        self.app_data = {
+            "env_file": self.load_env_file(),
+            "env_os": dict(sorted(env_os.items(), key=lambda x: x[0]))
+        }
+
         # load model
-        os_vars = list(sorted(os.environ.items(), key=lambda x: x[0]))
-        self.model = TableModel(os_vars)
+        # os_vars = list(sorted(os.environ.items(), key=lambda x: x[0]))
+        self.model = TableModel(self.app_data.get("env_os", {}))
         self.main_ui.table.setModel(self.model)
 
-        
+    def _create_configs(self, config_path: Path):
+        with config_path.open("w", encoding="utf-8") as iobj:
+            json.dump(self.app_configs, iobj)
+            iobj.close()
+
+    def _load_configs(self, config_path: Path):
+        with config_path.open("r", encoding="utf-8") as iobj:
+            self.app_configs = json.load(iobj)
+            iobj.close()
+
+    def _filter_vars(self, x):
+        exclude_patterns = re.compile(r"|".join(self.app_configs["excludes"]))
+        if exclude_patterns.match(x[0]):
+            return False
+        return x
+
+    def load_env_file(self):
+        pattern = re.compile(r'([_A-Za-z0-9]+?)="(.*)"|([_A-Za-z0-9]+?)=(.*)')
+
+        env = dict()
+
+        if self.env_file_path.exists():
+            with self.env_file_path.open("r", encoding="utf-8") as f:
+                for line in f.readlines():
+                    if line.startswith("#"):
+                        continue
+                    matched = pattern.search(line.strip())
+                    env[matched.group(1).strip()] = matched.group(2).strip()
+                f.close()
+
+        return env
+
+    def save_to_file(self):
+        with self.env_file_path.open("w", encoding="utf-8") as f:
+            if self.app_data["env_file"]:
+                for k, v in self.app_data["env_file"].items():
+                    f.write(f'{k}="{v}"\n')
+
+            f.close()
+
+   
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = MainWidget()
